46f377f4fcf05d99036736f90ac1a3b4a2dd6403
WesleyAnibal <wesley.anibal@ccc.ufcg.edu.br>
2016-11-23 01:50:14 -0300

    Selection Simultaneo

--- Atividade_13/src/sortings/SelectionSortSimultaneo.java
+++ Atividade_13/src/sortings/SelectionSortSimultaneo.java
@@ -5,32 +5,34 @@ import java.util.Arrays;
 public class SelectionSortSimultaneo {
 	public void SelectionSort(int[] v, int ini, int fina) {
 		if (ini < fina) {
-			int inicio = ini;
-			int fim = fina;
 			int maior = ini;
 			int menor = ini;
-			for (int i = ini; i <= fina; i++) {
-				if(v[i] < v[menor]){
-					menor = i;
-				}if(v[i] > v[maior]){
+			for (int i = maior + 1; i <= fina; i++) {
+				if (v[i] > v[maior]) {
 					maior = i;
 				}
 			}
-			
-			int aux = v[fim];
-			v[fim] = v[maior];
-			v[maior] = aux;
-			int aux2 = v[inicio];
-			v[inicio] = v[menor];
-			v[menor] = aux2;
-			ini++;
+			swap(v, maior, fina);
 			fina--;
-			SelectionSort(v,ini ,fina );
+			for (int i = fina; i > ini; i--) {
+				if (v[i] < v[menor]) {
+					menor = i;
+				}
+			}
+			swap(v, ini, menor);
+			ini++;
+			SelectionSort(v, ini, fina);
 		}
 	}
 
+	private void swap(int[] v, int inicio, int menor) {
+		int aux2 = v[inicio];
+		v[inicio] = v[menor];
+		v[menor] = aux2;
+	}
+
 	public static void main(String[] args) {
-		int[] x = { 9, 4, -10, 8, 5, -3, 0, 0 };
+		int[] x = { 0,0,0,0,0};
 		new SelectionSortSimultaneo().SelectionSort(x, 0, x.length - 1);
 		System.out.println(Arrays.toString(x));
 	}
